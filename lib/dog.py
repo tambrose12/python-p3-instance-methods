ce02f62d78647f3945f1104ad70818d548452f8b
Taylor Ambrose <taykesby@gmail.com>
2023-03-14 21:22:24 -0400

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,32 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
+    # Instance method definition
     pass
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+
+
+class Person:
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+guido = Person()
+guido.talk()
--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,15 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
+    # Instance method definition
     pass
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+guido = Person()
+guido.talk()
